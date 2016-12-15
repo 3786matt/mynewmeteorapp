061d381c57929b4a2576d4312cfd0f04032d2928
3786matt <3786matt@gmail.com>
2016-12-14 19:59:39 -0500

    more materialize/meteor added

--- server/main.js
+++ server/main.js
@@ -1,5 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
+  BrowserPolicy.content.allowOriginForAll('*');
   // code to run on server at startup
+
+  if(Meteor.users.findOne(Meteor.settings.adminId)){
+    Roles.addUsersToRoles(Meteor.settings.adminId, ['admin']);
+  }
 });
+
+
+
+
