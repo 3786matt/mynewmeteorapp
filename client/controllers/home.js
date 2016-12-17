bbd430849a215d4a7d13a6cc348d44721696e5c2
3786matt <3786matt@gmail.com>
2016-12-17 12:30:56 -0500

    Added delete, likes, sorting

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,6 +1,13 @@
+// Template.home.helpers({
+//   // check if user is an admin
+//   'post': function() {
+//     return Posts.find();
+//   }
+// });
+
 Template.home.helpers({
   // check if user is an admin
   'post': function() {
-    return Posts.find();
+    return Posts.find({}, {sort: {likes: -1} });
   }
 });
--- server/postmethods.js
+++ server/postmethods.js
@@ -12,6 +12,34 @@
 //   }
 // });
 
+// Meteor.methods({
+//   addPost: function (data) {
+//     Posts.insert({
+//       image: data,
+//       createdAt: new Date().toLocaleString(),
+//       likes: 0,
+//       user: {
+//         _id: Meteor.user()._id,
+//         email: Meteor.user().emails[0].address
+//       }
+//     });
+//   },
+//   deletePost: function(postId){
+//     var post = Posts.findOne({_id: postId }),
+//     postUserId = post.user._id;
+//     currentUserId = Meteor.userId();
+//     if(postUserId === currentUserId) {
+//       console.log("User deleted post.")
+//       Posts.remove({_id: postId});
+//     } else if(Roles.userIsInRole(Meteor.user(), ['admin'])){
+//       console.log("Admin deleted post.")
+//       Posts.remove({_id: postId});
+//     } else {
+//       console.log("Someone's trying to delete posts and shouldn't be.")
+//     }
+//   }
+// });
+
 Meteor.methods({
   addPost: function (data) {
     Posts.insert({
@@ -23,6 +51,12 @@ Meteor.methods({
         email: Meteor.user().emails[0].address
       }
     });
+  },
+  likePost: function(postId){
+    userSignedIn = Meteor.user() || false;
+    if(userSignedIn){
+      Posts.update({_id: postId}, {$inc: {likes: 1} });
+    }
   },
   deletePost: function(postId){
     var post = Posts.findOne({_id: postId }),
